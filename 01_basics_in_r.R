4cc31b7274ce77745fb4a17da5a2018b94579517
VaishnitaRandive <vaishnitarandive91@gmail.com>
2025-12-23 16:37:14 +0530

    Add files via upload

---
+++ 01_basics_in_r.R
@@ -0,0 +1,32 @@
+# we add 2 in 3 ----
+2+3
+#Arithmatic Operatores
+
+x = 2
+y = 3
+x+y
+
+y-x
+x-y
+x*y
+x/y
+x^y
+2*2*2
+x/9
+x/8
+x%%y
+### Some other operators ----
+a <- 2
+b <- 4+2
+b
+print(b)
+
+### Plot ----
+x = c( 1,2,3,4,5)
+y = c (1,4,9,16,8)
+plot(x,y)
+
+
+
+
+
